aceb8eec04103cb560dff764b62d2bb565c5091b
dostuffthatmatters <moritz@dostuffthatmatters.dev>
2022-07-14 13:22:00 +0200

    Add bad example

---
+++ bad-example/main.py
@@ -0,0 +1,7 @@
+API_USER_ID = "some-account-from-esm"
+API_SECRET_KEY = "abcde12345"
+
+print(
+    f'Connecting to API using USER_ID "{API_USER_ID}" '
+    + f'and SECRET_KEY "{API_SECRET_KEY}".'
+)
